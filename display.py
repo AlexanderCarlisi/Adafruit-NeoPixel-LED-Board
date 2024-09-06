33fa22b0038e0e8cbf4b6e48d9cdc4a7ef8e2ddf
Alexander Carlisi <alexandercarlisi0@gmail.com>
2024-09-06 17:30:23 -0400

    initial Commit, includes Display.py and snake_game.py

---
+++ display.py
@@ -0,0 +1,38 @@
+from rpi_ws281x import PixelStrip
+
+class Display:
+
+    LED_ROW = 10            # Rows of LEDs
+    LED_COLUMN = 34         # Columns of LEDs
+    LED_PIN = 18            # GPIO PWM
+    LED_FREQ_HZ = 800000    # 800khz
+    LED_DMA = 10            # DMA channel for generating a signal
+    LED_CHANNEL = 0         # Set to '1' for GPIOs 13, 19, 41, 45, or 53
+    
+    LED_MIN_VALUE = 0       # Minimum value for RGB and Brightness
+    LED_MAX_VALUE = 255     # Maximum value for RGB and Brightness
+
+    pixelStrip = PixelStrip(LED_ROW * LED_COLUMN, LED_PIN, LED_FREQ_HZ, LED_DMA, False, 20, LED_CHANNEL)
+    board = None
+
+
+    def __init__(self):
+        self.pixelStrip.begin() # Initialize rpi_ws281x Library
+        self.board = self.generate_board_map() # Initialize LedBoard Pin Mappings
+
+
+    def generate_board_map(self):
+        board_map = []
+        for row in range(self.LED_ROW):
+            if row % 2 == 0:
+                board_map.append(list(range(row * self.LED_COLUMN + self.LED_COLUMN - 1, row * self.LED_COLUMN - 1, -1)))
+            else:
+                board_map.append(list(range(row * self.LED_COLUMN, (row + 1) * self.LED_COLUMN)))
+
+        return board_map
+
+    def set_pixel_color(self, row, column, color):
+        self.pixelStrip.setPixelColorRGB(self.board[row][column], color.r, color.g, color.b, color.w)
+
+    def show(self):
+        self.pixelStrip.show()
---
+++ snake_game.py
@@ -0,0 +1,101 @@
+from display import Display
+from rpi_ws281x import Color
+from random import randint
+from enum import Enum
+import time
+import keyboard
+
+class Direction(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+APPLE_COLOR = Color(255, 0, 0, 20)
+APPLE_START_POSITION = (4, 25)
+
+SNAKE_COLOR = Color(0, 255, 0, 25)
+SNAKE_START_POSITION = (4, 5)
+
+BACKGROUND_COLOR = Color(0, 0, 0, 0)
+
+DISPLAY = Display()
+
+apple_position = APPLE_START_POSITION
+snake_head_position = SNAKE_START_POSITION
+snake_body_positions = [(SNAKE_START_POSITION[0], SNAKE_START_POSITION[1] - 1)] # Initialize with one body part
+
+input_direction = Direction.RIGHT
+previous_input_direction = Direction.RIGHT
+
+update_interval_seconds = 0.34
+last_update_time = time.time()
+
+score = 0
+
+
+# Input Listeners
+keyboard.add_hotkey('w', lambda: assign_direction(Direction.UP))
+keyboard.add_hotkey('s', lambda: assign_direction(Direction.DOWN))
+keyboard.add_hotkey('a', lambda: assign_direction(Direction.LEFT))
+keyboard.add_hotkey('d', lambda: assign_direction(Direction.RIGHT))
+
+def assign_direction(direction):
+    global input_direction, previous_direction
+    # Prevent reverse movement (180-degree turn)
+    if (direction == Direction.UP and previous_direction != Direction.DOWN) or \
+       (direction == Direction.DOWN and previous_direction != Direction.UP) or \
+       (direction == Direction.LEFT and previous_direction != Direction.RIGHT) or \
+       (direction == Direction.RIGHT and previous_direction != Direction.LEFT):
+        input_direction = direction
+
+
+# Main Loop
+while True:
+    DISPLAY.set_pixel_color(apple_position[0], apple_position[1], APPLE_COLOR) # Add new apple
+    if (time.time() - last_update_time) >= update_interval_seconds:
+        last_update_time = time.time()
+
+        # Add old head position to body
+        previous_head_position = snake_head_position
+        snake_body_positions.insert(0, previous_head_position)
+
+        # Update Head to new position
+        if input_direction == Direction.UP:
+            snake_head_position = (snake_head_position[0] - 1, snake_head_position[1])
+        elif input_direction == Direction.DOWN:
+            snake_head_position = (snake_head_position[0] + 1, snake_head_position[1])
+        elif input_direction == Direction.LEFT:
+            snake_head_position = (snake_head_position[0], snake_head_position[1] - 1)
+        elif input_direction == Direction.RIGHT:
+            snake_head_position = (snake_head_position[0], snake_head_position[1] + 1)
+
+        # Remove last body position
+        old_body_pos = snake_body_positions.pop() # old head has been added, this should never be empty here
+
+        # Check Apple Collision
+        if snake_head_position == apple_position:
+            score += 1
+            snake_body_positions.append(old_body_pos) # Increase length with old body position
+            DISPLAY.set_pixel_color(apple_position[0], apple_position[1], BACKGROUND_COLOR) # Remove old apple
+            validPosition = False
+            while not validPosition: # This sucks, so I might store all possible positions and remove them from the list
+                apple_position = (randint(0, DISPLAY.LED_ROW - 1), randint(0, DISPLAY.LED_COLUMN - 1))
+                if apple_position not in snake_body_positions and apple_position != snake_head_position:
+                    validPosition = True
+            DISPLAY.set_pixel_color(apple_position[0], apple_position[1], APPLE_COLOR) # Add new apple
+
+        if snake_head_position[0] < 0 or snake_head_position[0] >= DISPLAY.LED_ROW or \
+        snake_head_position[1] < 0 or snake_head_position[1] >= DISPLAY.LED_COLUMN or \
+        snake_head_position in snake_body_positions:
+            print("Game Over, Score: " + str(score))
+            break
+
+        # Update Display
+        DISPLAY.set_pixel_color(old_body_pos[0], old_body_pos[1], BACKGROUND_COLOR)
+        for body_pos in snake_body_positions:
+            DISPLAY.set_pixel_color(body_pos[0], body_pos[1], SNAKE_COLOR)
+        DISPLAY.set_pixel_color(snake_head_position[0], snake_head_position[1], SNAKE_COLOR)
+        DISPLAY.show()
+
+        previous_direction = input_direction
